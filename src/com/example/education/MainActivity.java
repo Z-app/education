c4470d881e8488b8f0cc6df2c567952eea41f25d
Fran Valladares <frava356@student.liu.se>
2012-09-22 01:04:17 +0200

    Just a test

--- src/com/example/education/MainActivity.java
+++ src/com/example/education/MainActivity.java
@@ -17,4 +17,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    // Fran: Trying to upload something
 }
