f012c111f26372d89b9ca7983d29af8b173b35f2
Linus Back <linba708@student.liu.se>
2012-10-01 23:52:18 +0200

    Now seems to work, still need to implement changing the twitter feed and having the picture. Should not be to hard. But Lead Desginers also needs sleep.

--- src/com/example/education/TwitterActivity.java
+++ src/com/example/education/TwitterActivity.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -12,25 +13,98 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 public class TwitterActivity extends ListActivity {
+	
+	private ArrayAdapter<String> myArrayAdapter;
     /** Called when the activity is first created. */
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
-        
-        setListAdapter(new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, this.fetchTwitterPublicTimeline()));
+        myArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1);
+        setListAdapter(myArrayAdapter);
+        new DownloadTwitterFeed().execute();
     }
+    private class DownloadTwitterFeed extends AsyncTask {
+
+		@Override
+		protected Object doInBackground(Object... params) {
+	    	StringBuilder builder = new StringBuilder();
+			HttpClient client = new DefaultHttpClient();
+	        HttpGet httpGet = new HttpGet("http://twitter.com/statuses/user_timeline/vogella.json");
+	        
+	        ArrayList<String> listItems = new ArrayList<String>();
+	        
+	        
+	        try {
+	            HttpResponse response = client.execute(httpGet);  //runtime error here, permission errors?
+	           
+	            StatusLine statusLine = response.getStatusLine();
+	            int statusCode = statusLine.getStatusCode();
+	            if (statusCode == 200) {
+	              HttpEntity entity = response.getEntity();
+	              InputStream content = entity.getContent();
+	              BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+	              String line;
+
+	              while ((line = reader.readLine()) != null) {
+	            	  builder.append(line);
+	            	 
+	              }
+	              content.close();
+	            } else {
+	              //Log.e(ParseJSON.class.toString(), "Failed to download file");
+	            }
+	          } catch (ClientProtocolException e) {
+	            e.printStackTrace();
+	          } catch (IOException e) {
+	            e.printStackTrace();
+	          }
+
+			try {
+				JSONArray jsonArray = new JSONArray(builder.toString());
+
+		        for (int i = 0; i<jsonArray.length(); i++){
+		        	listItems.add(jsonArray.getJSONObject(i).getString("text"));
+		        }
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	        return listItems;
+		}
+		
+		@Override
+		protected void onPostExecute(Object result){
+			myArrayAdapter.addAll((Collection<? extends String>) result);
+			
+			
+		}
+		
+		
+		
+    }
+    
+    
     
     //Det här skulle behöva göras i en separat tråd, antagligen som en subaktivitet.
     //se ndroid.os.NetworkOnMainThreadException
-    public ArrayList<String> fetchTwitterPublicTimeline()
-    {
+    /**
+     * DEPRICATED
+     * Was used to fetch a ArrayList.
+     * @return
+     */
+/*    public ArrayList<String> fetchTwitterPublicTimeline(){
     	
     	HttpClient client = new DefaultHttpClient();
         HttpGet httpGet = new HttpGet("http://twitter.com/statuses/user_timeline/vogella.json");
@@ -57,5 +131,5 @@ public class TwitterActivity extends ListActivity {
             e.printStackTrace();
           }
         return listItems;
-    }
+    }*/
 }
