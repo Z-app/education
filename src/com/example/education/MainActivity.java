3c3ac3f2e4f64cb3cfa79593c5cccb9a62c607ec
ChristianVestman <chrve033@student.liu.se>
2012-10-08 13:19:26 +0200

    test commentar

--- src/com/example/education/MainActivity.java
+++ src/com/example/education/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+//test
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
